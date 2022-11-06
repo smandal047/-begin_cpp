a12803fe7c43f7ae3951fd33078f76b14b7812fc
Sayantan Mandal <sanu_xze@yahoo.com>
2022-11-06 13:48:31 +0530

    Added cin problem

--- cin_input.cpp
+++ cin_input.cpp
@@ -7,6 +7,26 @@ using namespace std;
 
 int main(){
 
+    int input;
 
-    
+
+    do {
+        printf("Enter an integer, or -1 to quit\n");
+
+        if (! (cin >> input) ){
+            printf("Wrong input ;( Try Again\n");
+            cin.clear();
+            cin.ignore();
+        }
+        
+        if (input > 0){
+            cout << "you entered: " << input << endl;
+            cout << "--------------" << endl;
+        }
+        
+    }
+    while( input > 0 );
+
+    cout << "Exiting..." << endl;
+
 }
